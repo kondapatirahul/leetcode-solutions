c59d5fe255172428607c7d6174373a348d02e423
RahulTechLearner <kondapatirahul@gmail.com>
2026-06-01 12:39:39 +0000

    Sync LeetCode submission - Maximum Subarray - Runtime - 1 ms (99.95%), Memory - 77.1 MB (65.38%)

---
+++ solutions/0053-maximum-subarray/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int n=nums.length;
+        int sum=0;
+        int max=Integer.MIN_VALUE;
+        if(n>1)
+        {
+        for(int i=0;i<n;i++)
+        {
+            sum+=nums[i];
+            max=Math.max(sum,max);
+            if(sum<0)
+            {
+                sum=0;
+            }
+        }
+        }
+        else{
+            max=nums[0];
+        }
+        return max;
+        
+    }
+}
