81a1b74cb187ea8eafa24c87df02928c07f45cfb
RahulTechLearner <kondapatirahul@gmail.com>
2026-06-09 04:02:33 +0000

    Sync LeetCode submission - Partition Equal Subset Sum - Runtime - 31 ms (81.84%), Memory - 43.9 MB (80.88%)

---
+++ solutions/0416-partition-equal-subset-sum/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+     int total=0;
+     for(int i=0;i<nums.length;i++) total+=nums[i];
+     if(total%2!=0)
+     {
+        return false;
+     }
+     int sub=total/2;
+     boolean dp[]=new boolean[sub+1];
+     dp[0]=true;
+     for(int num:nums)
+     {
+        for(int i=sub;i>=num;i--)
+        {
+            dp[i]=dp[i]||dp[i-num];
+        }
+     }
+            return dp[sub];
+        
+    }
+}
