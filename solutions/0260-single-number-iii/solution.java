37fdb6f46ea71e2c67ac9fc2243dd0dc12b7c125
RahulTechLearner <kondapatirahul@gmail.com>
2026-06-13 05:50:18 +0000

    Sync LeetCode submission - Single Number III - Runtime - 351 ms (5.08%), Memory - 48.1 MB (22.59%)

---
+++ solutions/0260-single-number-iii/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int arr[]=new int[2];
+        int l=0;
+        for(int i=0;i<nums.length;i++)
+        {
+        int count=0;
+            for(int j=0;j<nums.length;j++)
+            {
+                if(nums[i]==nums[j])
+                {
+                    count++;
+                }
+            }
+            if(count==1)
+            {
+                arr[l]=nums[i];
+                l++;
+            }
+        }
+       
+        
+        return arr;
+
+    }
+}
