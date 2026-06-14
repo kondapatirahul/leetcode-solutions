e613d368d59ecfd77118a89a4dd7231bc3322e82
RahulTechLearner <kondapatirahul@gmail.com>
2026-06-14 14:56:47 +0000

    Sync LeetCode submission - Power of Four - Runtime - 1 ms (89.69%), Memory - 42.6 MB (50.65%)

---
+++ solutions/0342-power-of-four/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n<=0)
+        {
+            return false;
+        }
+        else if((n&(n-1))==0 && n%3==1 )
+        {
+            return true;
+        }
+        return false;
+    }
+}
