486cb63a24fc342b06ea934bd04565d054986c4f
RahulTechLearner <kondapatirahul@gmail.com>
2026-06-18 08:37:11 +0000

    Sync LeetCode submission - Sqrt(x) - Runtime - 1 ms (97.76%), Memory - 42.5 MB (77.82%)

---
+++ solutions/0069-sqrtx/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int mySqrt(int x) {
+        int l=1;
+        int h=x;
+        while(l<=h)
+        {
+            int m=l+(h-l)/2;
+            if((long)m*m <=x)
+            {
+                l=m+1;
+            }
+            else{
+                h=m-1;
+            }
+        }
+        return h;       
+    }
+}
