0786a096878b7b6ec3b4ab111625244dfbde3226
RahulTechLearner <kondapatirahul@gmail.com>
2026-06-20 12:47:04 +0000

    Sync LeetCode submission - Find All Duplicates in an Array - Runtime - 24 ms (26.25%), Memory - 72 MB (21.15%)

---
+++ solutions/0442-find-all-duplicates-in-an-array/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        HashSet<Integer> hs=new HashSet<>();
+        List<Integer> ans=new ArrayList<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            if(hs.contains(nums[i]))
+            {
+                ans.add(nums[i]);
+            }
+            else{
+                hs.add(nums[i]);
+                
+            }
+        }
+        return ans;
+
+    }
+}
