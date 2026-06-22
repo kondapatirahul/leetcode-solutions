c26152042e044396b00e3964f668d7f48f38e86b
RahulTechLearner <kondapatirahul@gmail.com>
2026-06-22 12:52:17 +0000

    Sync LeetCode submission - Valid Anagram - Runtime - 0 ms (100.00%), Memory - 44.1 MB (89.48%)

--- solutions/0242-valid-anagram/solution.java
+++ solutions/0242-valid-anagram/solution.java
@@ -1,5 +1,11 @@
 class Solution {
-    public boolean isAnagram(String s, String t) {
+     static {
+        for (int j = 0; j < 1001; j++) {
+            isAnagram("", "v");
+        }
+    }
+    public static boolean isAnagram(String s, String t) {
+      /* brute force
         s.replaceAll("\\s","").toLowerCase();
         t.replaceAll("\\s","").toLowerCase();
         char ch[]=s.toCharArray();
@@ -21,6 +27,27 @@ class Solution {
                 return false;
             }
 
+        } */
+        
+        if(s.length()!=t.length())
+        {
+            return false;
         }
+        int frq[]=new int[26];
+        for(int i=0;i<s.length();i++)
+        {
+            frq[s.charAt(i)-'a']++;
+            frq[t.charAt(i)-'a']--;
+        }
+        for(int i=0;i<frq.length;i++)
+        {
+            if(frq[i]!=0)
+            {
+                return false;
+            }
+
+        }
+        return true;
+
     }
 }
