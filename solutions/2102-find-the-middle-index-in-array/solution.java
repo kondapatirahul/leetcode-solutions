962bada58830221fb44a859f99859768be598f99
RahulTechLearner <kondapatirahul@gmail.com>
2026-06-22 13:04:06 +0000

    Sync LeetCode submission - Find the Middle Index in Array - Runtime - 0 ms (100.00%), Memory - 43.8 MB (7.69%)

---
+++ solutions/2102-find-the-middle-index-in-array/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findMiddleIndex(int[] nums) {
+        int totalsum=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            totalsum+=nums[i];
+        }
+        int leftsum=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(leftsum==totalsum-leftsum-nums[i])
+            {
+                return i;
+            }
+            leftsum+=nums[i];
+        }
+        return -1;
+    }
+}
