e5a946544eaa9e0fdcd92da10f0a9a31230bc091
RahulTechLearner <kondapatirahul@gmail.com>
2026-07-18 16:37:36 +0000

    Sync LeetCode submission - Number of Steps to Reduce a Number in Binary Representation to One - Runtime - 2 ms (25.73%), Memory - 43.1 MB (14.67%)

---
+++ solutions/1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/solution.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int numSteps(String s) {
+        StringBuilder sb=new StringBuilder(s);
+        int count=0;
+        while(sb.length()>1)
+        {
+            if(sb.charAt(sb.length()-1)=='0')
+            {
+                sb.deleteCharAt(sb.length()-1);
+            }
+            else{
+                addOne(sb);
+            }
+            count++;
+        } 
+        return count;
+    }   
+    private void addOne(StringBuilder sb) {
+        int i = sb.length() - 1;
+        while (i >= 0 && sb.charAt(i) == '1') {
+            sb.setCharAt(i, '0');
+            i--;
+        }
+        if (i >= 0) {
+            sb.setCharAt(i, '1');
+        } else {
+            sb.insert(0, '1');
+        }
+    }
+}
+
