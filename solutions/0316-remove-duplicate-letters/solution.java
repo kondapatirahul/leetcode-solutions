6a87489d13227308fa45cb2b7041c4190578a38e
RahulTechLearner <kondapatirahul@gmail.com>
2026-07-19 06:41:33 +0000

    Sync LeetCode submission - Remove Duplicate Letters - Runtime - 4 ms (21.04%), Memory - 43.7 MB (68.95%)

---
+++ solutions/0316-remove-duplicate-letters/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String removeDuplicateLetters(String s) {
+        Stack<Character> sb = new Stack<>();
+        if (s.isEmpty()) return "";
+        sb.push(s.charAt(0));
+        for (int i=1;i<s.length();i++) {
+            char current=s.charAt(i);
+            if (sb.contains(current)) {
+                continue; 
+            }
+            while (!sb.isEmpty() && sb.peek() > current && s.indexOf(sb.peek(), i) != -1) {
+                sb.pop();
+            }
+            sb.push(current);
+        }
+        StringBuilder ans=new StringBuilder();
+        for (char ch : sb) {
+            ans.append(ch);
+        }
+        return ans.toString();
+    }
+}
