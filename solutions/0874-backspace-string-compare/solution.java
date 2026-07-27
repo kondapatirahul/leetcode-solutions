6bd123a2b0e96f9b908f6f6cea42ea85998a4341
RahulTechLearner <kondapatirahul@gmail.com>
2026-07-27 07:53:46 +0000

    Sync LeetCode submission - Backspace String Compare - Runtime - 3 ms (20.62%), Memory - 43.2 MB (39.33%)

---
+++ solutions/0874-backspace-string-compare/solution.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        Stack<Character> stack1=new Stack<>();
+        Stack<Character> stack2=new Stack<>();
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            if(ch=='#')
+            {
+                if(!stack1.empty())
+                {
+                    stack1.pop();
+                }
+
+            }
+            else{
+                stack1.push(ch);
+            }
+        }
+        for(int i=0;i<t.length();i++)
+        {
+            char ch=t.charAt(i);
+            if(ch=='#')
+            {
+                if(!stack2.empty())
+                {
+                    stack2.pop();
+                }
+            }
+            else{
+                stack2.push(ch);
+            }
+        }
+        return stack1.equals(stack2);
+    }
+}
