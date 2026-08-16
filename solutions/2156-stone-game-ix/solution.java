5feefe89145b48e4c12402cfccb0c6cc79eb12a4
RahulTechLearner <kondapatirahul@gmail.com>
2026-08-16 07:42:14 +0000

    Sync LeetCode submission - Stone Game IX - Runtime - 4 ms (75.50%), Memory - 114.6 MB (82.12%)

---
+++ solutions/2156-stone-game-ix/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+        int n=stones.length;
+        int cnt[]=new int[3];
+        for(int i=0;i<n;i++) cnt[stones[i]%3]++;
+        if(cnt[0]%2==0)
+        {
+            return cnt[1]>0 && cnt[2]>0;
+        }
+        return Math.abs(cnt[1]-cnt[2])>2;
+        
+    }
+}
