5cb90f6dac6b6a2d6cb53e0cec21dde204b7a467
RahulTechLearner <kondapatirahul@gmail.com>
2026-08-24 16:43:23 +0000

    Sync LeetCode submission - Stone Game VIII - Runtime - 4 ms (67.09%), Memory - 87.1 MB (50.43%)

---
+++ solutions/2002-stone-game-viii/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int stoneGameVIII(int[] stones) {
+        int n=stones.length;
+        long prefix[]=new long[n];
+        prefix[0]=stones[0];
+        for(int i=1;i<n;i++)
+        {
+            prefix[i]=stones[i]+prefix[i-1];
+        }
+        long ans=prefix[n-1];
+        for(int i=n-2;i>=1;i--)
+        {
+            ans=Math.max(ans,prefix[i]-ans);
+        }
+        return (int) ans;
+    }
+}
